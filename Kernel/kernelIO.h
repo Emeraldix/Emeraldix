76906b9aaac452677ac40b862baaa87e3273aed0
Emeraldix <67229081+Emeraldix@users.noreply.github.com>
2020-07-27 19:43:10 +0300

    Add system font and background colors

--- Kernel/kernelIO.h
+++ Kernel/kernelIO.h
@@ -1,5 +1,17 @@
 #define VIDEOMEM 0xb8000
 
+//Colors
+#define BLACK {0, 0, 0}
+#define RED {255, 0, 0}
+#define YELLOW {255, 255, 0}
+#define GREEN {0,  255, 0}
+#define CYAN {0, 255, 255}
+#define BLUE {0, 0, 255}
+#define MAGENTA {255, 0, 255}
+#define WHITE {255, 255, 255}
+#define EMERALD {11, 179, 141}
+
+
 /**
  * Выводит сообщение на экран
  * @param msg Выводимое сообщение
